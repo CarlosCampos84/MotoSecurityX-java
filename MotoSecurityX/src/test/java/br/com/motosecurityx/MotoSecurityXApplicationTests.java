1cf0d0c50f421ad9f999b24e5a1a0b5da723395c
CarlosCampos84 <caduferrazcampos04@gmail.com>
2025-09-25 19:17:03 -0300

    chore: inicializa projeto Spring Boot MotoSecurityX

---
+++ MotoSecurityX/src/test/java/br/com/motosecurityx/MotoSecurityXApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.motosecurityx;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MotoSecurityXApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
